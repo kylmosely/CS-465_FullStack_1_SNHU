00795a18b1d69c4ce63981e4ac3fa0ec8f8d0043
kylmosely <99621533+kylmosely@users.noreply.github.com>
2023-06-03 20:37:19 -0500

    added api - access mongo
    Had to rebuild much of the project, as my original pc had some issues this week.

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const hbs = require('hbs');
+require('./app_api/models/db');
+
+var aboutRouter = require('./app_server/routes/about');
+var contactRouter = require('./app_server/routes/contact');
+var indexRouter = require('./app_server/routes/index');
+var mealsRouter = require('./app_server/routes/meals');
+var newsRouter = require('./app_server/routes/news');
+var roomsRouter = require('./app_server/routes/rooms');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+app.use('/index', indexRouter);
+app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/rooms', roomsRouter);
+app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname,'public')));
-
-app.listen(PORT, () => console.log('Server listening on port ' + PORT));
